1110ad5684260939f1ad1faed35909fe08d2b2c9
Joello61 <tchindajoel61@gmail.com>
2025-11-08 01:33:46 +0100

    feat: Ajouter la prise en charge de Google Analytics avec la variable d'environnement NEXT_PUBLIC_GA_ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -197,6 +197,19 @@ export default function RootLayout({
           }}
         />
 
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `,
+          }}
+        />
+
         <ClientRootProvider>{children}</ClientRootProvider>
       </body>
     </html>
